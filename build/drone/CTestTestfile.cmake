d27b6427333cc652c5dd4c6dd2aa934195114532
mertgungor <mmmertgungor@gmail.com>
2022-02-16 08:59:55 +0300

    takeoff

---
+++ build/drone/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/mert/circle/src/drone
+# Build directory: /home/mert/circle/build/drone
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
